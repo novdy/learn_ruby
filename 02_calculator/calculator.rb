1aaa7510c95e8d630942b45cf715a57e33f06ca0
novdy <peter3hayes@gmail.com>
2019-03-20 20:49:45 -0500

    02_calculator completed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,52 @@
-#write your code here
+def add(num1, num2)
+	return num1 + num2
+end
+
+def subtract(num1, num2)
+	return num1 - num2
+end
+
+def sum(numbers)
+	total = 0
+	numbers.each do |num|
+		total = add(total, num)
+	end
+	return total
+end
+
+# This function goes beyond the scope of the problem. Multiples from arrays recursively
+# Example: multiply([2,10,[78,[102,13]],5], 15, [12,4,6])
+def multiply(*args)
+	total = 1
+	args.each do |num|
+		if num.kind_of?(Array)
+			if num.length == 0; return 0 end
+
+			ar_total = num[0]			
+			i = 1
+			until i == num.length 
+				ar_total = multiply(ar_total, num[i])	
+				i = i + 1
+			end
+			num = ar_total
+		end
+		total = total * num
+	end
+	return total
+end
+
+def power(num, power)
+	total = 1
+	power.times do 
+		total = total * num
+	end
+	return total
+end	
+
+def factorial(num)
+	if num <= 0
+		return 1
+	else
+		return num * factorial(num - 1)
+	end
+end
--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -79,21 +79,52 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+	  expect(multiply(2, 4)).to eq(8)
+  end
 
-  it "multiplies several numbers"
-  
+  it "multiplies multiple numbers" do
+	  expect(multiply(3,5,7)).to eq(105)
+  end
+
+  it "multiplies from an empty array" do
+	  expect(multiply([], 3)).to eq(0)
+  end
+
+  it "multiplies from 1 array" do
+	  expect(multiply([2,3,4])).to eq(24)
+  end
+
+  it "multiplies multiple arrays" do 
+	  expect(multiply([2,4],[10])).to eq(80)
+  end
+
+  it "multiplies nested arrays" do
+	  expect(multiply([2, [3,5]])).to eq(30)
+  end
 end
 
 describe "#power" do
-  it "raises one number to the power of another number"
+  it "raises one number to the power of another number" do
+	  expect(power(2,10)).to eq(1024)
+  end
 end
 
 # http://en.wikipedia.org/wiki/Factorial
 describe "#factorial" do
-  it "computes the factorial of 0"
-  it "computes the factorial of 1"
-  it "computes the factorial of 2"
-  it "computes the factorial of 5"
-  it "computes the factorial of 10"
+  it "computes the factorial of 0" do
+	  expect(factorial(0)).to eq(1)
+  end
+  it "computes the factorial of 1" do
+	  expect(factorial(1)).to eq(1)
+  end
+  it "computes the factorial of 2" do
+	  expect(factorial(2)).to eq(2)
+  end
+  it "computes the factorial of 5" do
+	  expect(factorial(5)).to eq(120)
+  end
+  it "computes the factorial of 10" do
+	  expect(factorial(10)).to eq(3628800)
+  end
 end
