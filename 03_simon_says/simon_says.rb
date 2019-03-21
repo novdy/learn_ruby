efed4097300d519412a724a8b33e792568a861f5
novdy <peter3hayes@gmail.com>
2019-03-20 22:24:37 -0500

    03_simon_says completed

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
-#write your code here
+def echo(str)
+	return str
+end
+
+def shout(str)
+	return str.upcase
+end
+
+def repeat(str, times = 2)
+	return ((str + " ") * times).strip
+end
+
+def start_of_word(str, place)
+	return str[0..(place-1)]
+end
+
+def first_word(words)
+	return words.split[0]
+end
+
+def titleize(words)
+	little_words = ['a', 'an', 'the', 'and', 'but', 'or', 'for', 'nor', 'on', 'at', 'to', 'from', 'by', 'over']
+	words = words.split
+	for word_num in 0..(words.length - 1) do
+		if word_num == 0 || !little_words.include?(words[word_num])
+			words[word_num] = words[word_num].capitalize
+		end
+	end
+	return words.join(" ")
+end
