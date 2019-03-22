38ec4e532bf27bbee3b6150b3ec07c82c75958a0
novdy <peter3hayes@gmail.com>
2019-03-21 23:02:26 -0500

    06_timer completed

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,29 @@
 class Timer
-  #write your code here
+	attr_reader :time_string
+	attr_accessor :seconds
+
+	def initialize
+		@seconds = 0
+		@time_string = "00:00:00"
+	end
+
+	def seconds=(seconds)
+		@seconds = seconds
+
+		hours   = (seconds / 3600)
+		minutes = (seconds / 60) % 60
+		seconds = (seconds % 60)
+
+		@time_string = "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
+	end
+
+	private
+
+	def padded(time)
+		if time < 10
+			return "0#{time}"
+		else
+			return time.to_s
+		end
+	end
 end
